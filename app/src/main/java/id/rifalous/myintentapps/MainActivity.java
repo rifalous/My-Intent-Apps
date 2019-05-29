f9f091c9524daf2e1c43c98fec83fa03c46de02b
Rivaldy Fauzan <rifalous@gmail.com>
2019-05-29 12:40:09 +0700

    Initial Commit

---
+++ app/src/main/java/id/rifalous/myintentapps/MainActivity.java
@@ -0,0 +1,31 @@
+package id.rifalous.myintentapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnMoveActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this,MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
