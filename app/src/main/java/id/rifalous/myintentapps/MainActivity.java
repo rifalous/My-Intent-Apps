c63b4a2967f26b7fb056c8dadd80b64edfdd2da3
Rivaldy Fauzan <rifalous@gmail.com>
2019-06-25 15:07:26 +0700

    Add Explicit Intent with Parcelable

--- app/src/main/java/id/rifalous/myintentapps/MainActivity.java
+++ app/src/main/java/id/rifalous/myintentapps/MainActivity.java
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        switch (v.getId()){
             case R.id.btn_move_activity:
-                Intent moveIntent = new Intent(MainActivity.this,MoveActivity.class);
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
                 break;
             case R.id.btn_move_activity_data:
@@ -39,22 +39,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
                 startActivity(moveWithDataIntent);
-                break;/*
+                break;
             case R.id.btn_move_with_object:
                 Person person = new Person();
                 person.setName("DicodingAcademy");
                 person.setAge(5);
                 person.setEmail("academy@dicoding.com");
                 person.setCity("Bandung");
-
                 Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
                 startActivity(moveWithObjectIntent);
-                break;*/
-            case R.id.btn_dial_number:
-                String phoneNumber = "082218392248";
-                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
-                startActivity(dialPhoneIntent);
                 break;
         }
     }
--- app/src/main/java/id/rifalous/myintentapps/MoveWithObjectActivity.java
+++ app/src/main/java/id/rifalous/myintentapps/MoveWithObjectActivity.java
@@ -2,14 +2,20 @@ package id.rifalous.myintentapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MoveWithObjectActivity extends AppCompatActivity {
-
-    public static final Object EXTRA_PERSON = "extra_person";
+    public static final String EXTRA_PERSON = "extra_person";
+    TextView tvObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_move_with_object);
+        tvObject = findViewById(R.id.tv_object_received);
+
+        Person person = getIntent().getParcelableExtra(EXTRA_PERSON);
+        String text = "Name : " + person.getName() + ",\nEmail : " + person.getEmail() + ",\nAge : " + person.getAge() + ",\nLocation : " + person.getCity();
+        tvObject.setText(text);
     }
 }
